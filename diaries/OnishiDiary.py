8c33b94359b9367e2fde2b9ab564923cc32357f9
k20024 <k20024kk@aitech.ac.jp>
2021-12-09 16:43:34 +0900

    Update OnishiDiary.py

--- diaries/OnishiDiary.py
+++ diaries/OnishiDiary.py
@@ -6,7 +6,7 @@ class DiarySample(AbstractDiary):
         return "2021-12-09"
 
     def get_summary(self):
-        return "今日初のグループワークが始まりその難易度どの高さに絶望しているところだ。"
+        return "今日初のグループワークが始まりその難易度の高さに絶望しているところだ。"
 
     def get_author(self):
         return "Onishi"
